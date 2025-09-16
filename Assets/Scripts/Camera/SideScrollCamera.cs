44a9ac153270adbb73e9ca1c05a014ad7256a709
Juan Digilio <juanignaciodigilio@gmail.com>
2025-09-16 13:38:34 -0300

    Added CameraManager

---
+++ Assets/Scripts/Camera/SideScrollCamera.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SideScrollCamera : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float screenThreshold = 0.75f;
+
+    private Camera mainCamera;
+    private float lastCameraX;
+
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        lastCameraX = mainCamera.transform.position.x;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 viewportPos = mainCamera.WorldToViewportPoint(playerTransform.position);
+
+        if (viewportPos.x > screenThreshold)
+        {
+            float deltaX = playerTransform.position.x - mainCamera.ViewportToWorldPoint(new Vector3(screenThreshold, viewportPos.y, viewportPos.z)).x;
+            float newCameraX = transform.position.x + deltaX;
+
+            transform.position = new Vector3(Mathf.Max(newCameraX, lastCameraX), transform.position.y, transform.position.z);
+            lastCameraX = transform.position.x;
+        }
+    }
+}
